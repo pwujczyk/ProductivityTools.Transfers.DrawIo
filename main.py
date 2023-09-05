fc4252c967fe99f1f2c0d2c07dd6db0da69aff12
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-09-05 20:48:45 +0200

    iterate

---
+++ main.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def process_file():
+    tree=ET.parse(".\Money.drawio.xml")
+    mxfile=tree.getroot();
+    print(mxfile.tag);
+    root=mxfile[0][0][0]
+    print(mxfile[0][0][0].tag)
+    for element in root.iter('object'):
+        print(element.tag,element.attrib)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    process_file();
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
