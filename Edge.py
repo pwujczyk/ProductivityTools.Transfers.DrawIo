8f8f2dc80fd68b7bb6fd837749517554a146be0e
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-09-07 12:19:01 +0200

    edges

---
+++ Edge.py
@@ -0,0 +1,4 @@
+class Edge:
+    def __init__(self, transferDay, value):
+        self.transferDay=transferDay
+        self.value=value
--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import xml.etree.ElementTree as ET
+from Edge import Edge
+
+edges = []
+vertexes = []
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def process_file():
-    tree=ET.parse(".\Transfers.drawio.xml")
-    mxfile=tree.getroot();
+def loadxml():
+    tree = ET.parse(".\Transfers.drawio.xml")
+    mxfile = tree.getroot();
     print(mxfile.tag);
-    root=mxfile[0][0][0]
+    root = mxfile[0][0][0]
     print(mxfile[0][0][0].tag)
     for element in root.iter('object'):
         if 'edge' in element[0].attrib:
-            print("Laczenie")
+           #
+            #edges.append(edge)
+            print(element.attrib["TransferDay"])
+            print(element.attrib["Value"])
+            edge = Edge(element.attrib["TransferDay"],element.attrib["Value"])
+            edges.append(edge)
+            print(element.attrib)
+            print("edge")
 
         if 'vertex' in element[0].attrib:
-            print("element")
+            vertexes.append(element[0])
+            #print("element")
+            #print(element.tag, element.attrib)
 
-        print(element.tag,element.attrib)
+def process_file():
+    loadxml()
+    print("amount of vertexes")
+    print(len(vertexes))
 
+    print("amount of edges")
+    print(len(edges))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
